0f5079ca9323a12df49adceb4f964e365a717521
Markus Gehmayer <markus.gehmayer@gmx.at>
2019-11-11 16:08:48 +0100

    product comp

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -6,6 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
+
+	advantages = [
+    {
+      id : 0,
+      fa : "",
+      title : "test",
+      descr : "Text",
+    },{
+      id : 1,
+      fa : "",
+      title : "test",
+      descr : "Text",
+    },{
+      id : 2,
+      fa : "",
+      title : "test",
+      descr : "Text",
+    }
+  ];
+
+	data = [
+    {
+      id : 0,
+      course : "Intro",
+      price : "EUR 299,--",
+      descr : "Intro Text",
+    },{
+      id : 1,
+      course : "Basic",
+      price : "EUR 499,--",
+      descr : "Basic Text",
+    },{
+      id : 2,
+      course : "Advanced",
+      price : "EUR 899,--",
+      descr : "Adv Text",
+    }
+  ];
   
   constructor() { }
 
