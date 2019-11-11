a9301eb8993c66671016b30d147c5a04ac065f88
Laszlo Sandor <voidlaszlo@gmail.com>
2019-11-11 11:54:32 +0100

    adding admin pages + docs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProductsComponent } from './pages/products/products.component';
 import { NewsComponent } from './pages/news/news.component';
 import { InformationComponent } from './pages/information/information.component';
+import { NewsadminComponent } from './pages/news/newsadmin/newsadmin.component';
+import { ProductsadminComponent } from './pages/products/productsadmin/productsadmin.component';
 
 
 const routes: Routes = [
   {path:'', component: ProductsComponent},
   {path:'news', component: NewsComponent},
-  {path:'information', component: InformationComponent}
+  {path:'information', component: InformationComponent},
+  {path:'news/admin', component: NewsadminComponent},
+  {path:'products/admin', component: ProductsadminComponent},
+
 ];
 
 @NgModule({
