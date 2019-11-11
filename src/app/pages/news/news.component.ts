74a4fcd590100e55861e1b0fa6b4e253effa42b1
Laszlo Sandor <voidlaszlo@gmail.com>
2019-11-11 13:06:28 +0100

    basic news structure

--- src/app/pages/news/news.component.ts
+++ src/app/pages/news/news.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news.component.scss']
 })
 export class NewsComponent implements OnInit {
-  
+  data = [
+    {
+      id : 0,
+      title : "Title",
+      author : "Author",
+      date : "September",
+      description : "Description",
+      body : "Body"
+    },
+  ]
   constructor() { }
 
   ngOnInit() {
