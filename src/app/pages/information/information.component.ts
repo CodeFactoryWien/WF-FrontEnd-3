a366561f98d6afdde9c68e28a6fe1a6c8fb67ba1
Barbara Anissin <anissinb@gmail.com>
2019-11-12 09:30:28 +0100

    show more and less button works

--- src/app/pages/information/information.component.ts
+++ src/app/pages/information/information.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { $ } from 'protractor';
 
 @Component({
   selector: 'app-information',
@@ -48,48 +47,48 @@ export class InformationComponent implements OnInit {
     {
       technologies: 'Angular',
       imgLinks: 'https://angular.io/assets/images/logos/angular/angular.png',
-      infos: '##',
+      infos: 'Angular is a platform and framework for building client applications in HTML and TypeScript. Angular is written in TypeScript. It implements core and optional functionality as a set of TypeScript libraries that you import into your apps.',
     },
     {
       technologies: 'Firebase',
       imgLinks: 'https://cdn4.iconfinder.com/data/icons/google-i-o-2016/512/google_firebase-2-512.png',
-      infos: '##',
+      infos: 'Firebase is a mobile and web application development platform developed by Firebase, Inc. in 2011, then acquired by Google in 2014. As of October 2018, the Firebase platform has 18 products, which are used by 1.5 million apps.',
     },
     {
       technologies: 'Sublime',
       imgLinks: 'https://i.imgur.com/AGmyyOX.png',
-      infos: '##',
+      infos: 'Sublime Text is a shareware cross-platform source code editor with a Python application programming interface (API). It natively supports many programming languages and markup languages, and functions can be added by users with plugins, typically community-built and maintained under free-software licenses.',
     },
     {
       technologies: 'Visual Studio',
       imgLinks: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/2000px-Visual_Studio_Code_1.35_icon.svg.png',
-      infos: '##',
+      infos: 'Visual Studio is an Integrated Development Environment(IDE) developed by Microsoft to develop GUI(Graphical User Interface), console, Web applications, web apps, mobile apps, cloud, and web services, etc. With the help of this IDE, you can create managed code as well as native code. It uses the various platforms of Microsoft software development software like Windows store, Microsoft Silverlight, and Windows API, etc. It is not a language-specific IDE as you can use this to write code in C#, C++, VB(Visual Basic), Python, JavaScript, and many more languages. It provides support for 36 different programming languages. It is available for Windows as well as for macOS.',
     },
     {
       technologies: 'Git',
       imgLinks: 'https://avatars2.githubusercontent.com/u/4571183?s=400&v=4',
-      infos: '##',
+      infos: 'Git ist eine freie Software zur verteilten Versionsverwaltung von Dateien, die durch Linus Torvalds initiiert wurde.',
     },
     {
       technologies: 'GitHub',
       imgLinks: 'https://cdn1.iconfinder.com/data/icons/logotypes/32/github-512.png',
-      infos: '##',
+      infos: 'GitHub provides open-source code hosting services.',
     },{
       technologies: 'Chrome',
       imgLinks: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Google_Chrome_icon_%282011%29.svg/2000px-Google_Chrome_icon_%282011%29.svg.png',
-      infos: '##',
+      infos: 'Google Chrome is a cross-platform web browser developed by Google. It was first released in 2008 for Microsoft Windows, and was later ported to Linux, macOS, iOS, and Android. The browser is also the main component of Chrome OS, where it serves as the platform for web apps.',
     },{
       technologies: 'Koala',
       imgLinks: 'https://d2.alternativeto.net/dist/icons/koala_29298.png?width=200&height=200&mode=crop&upscale=false',
-      infos: '##',
+      infos: 'Koala is a GUI application for Less, Sass, Compass and CoffeeScript compilation, to help web developers to use them more efficiently. Koala can run in windows, linux and mac.',
     },{
       technologies: 'CMD',
       imgLinks: 'https://icon-library.net/images/cmd-icon/cmd-icon-17.jpg',
-      infos: '##',
+      infos: 'A command-line interface (CLI) is a means of interacting with a computer program where the user (or client) issues commands to the program in the form of successive lines of text (command lines). The program which handles the interface is called a command-line interpreter or command-line processor.',
     },{
       technologies: 'Moodle',
       imgLinks: 'https://learnbps.bismarckschools.org/pluginfile.php/535022/mod_book/chapter/75350/M1200x800.png',
-      infos: '##',
+      infos: 'Moodle is a free and open-source learning management system (LMS) written in PHP and distributed under the GNU General Public License. Developed on pedagogical principles, Moodle is used for blended learning, distance education, flipped classroom and other e-learning projects in schools, universities, workplaces and other sectors.',
     },
   ]
   constructor() {
@@ -100,26 +99,16 @@ export class InformationComponent implements OnInit {
 
   
   show(e){
-    var button = e.target.parentNode.previousElementSibling.lastChild;
-    button.style.display=`none`;
-    var nextElem = e.target.parentNode;
-    console.log(nextElem)
+    var changeText = e.currentTarget
+    console.log(changeText);
+    var nextElem = e.target.previousElementSibling;
     if (nextElem.style.display==='none') { 
       nextElem.style.display='block'; 
-    }
-    else { 
-      nextElem.style.display='block'; 
-    }
-  }
-  hide(e) {
-    var nextElem = e.target.parentNode.nextElementSibling;
-    e.target.style.display=`block`;
-
-    if (nextElem.style.display==='block') { 
-      nextElem.style.display='none'; 
+      changeText.innerHTML = "Show less...";
     }
     else { 
       nextElem.style.display='none'; 
+      changeText.innerHTML = "Show more...";
     }
   }
 }
